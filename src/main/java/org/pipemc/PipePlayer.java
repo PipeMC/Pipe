822ed3570562513b6d386f1ff7249e13d6a0dc5e
Lior <Lior@DESKTOP-BJOM68R>
2017-07-18 21:08:43 +0300

    A

--- src/main/java/org/pipemc/PipePlayer.java
+++ src/main/java/org/pipemc/PipePlayer.java
@@ -1,6 +1,20 @@
 package org.pipemc;
 
+import org.bukkit.entity.Entity;
+
 public interface PipePlayer extends PipeEntity {
 
+	void setDisplayName(String name, boolean aboveHead, boolean tabList);
+	
+	void setStatus(byte status);
+	
+	void setStatus(Entity entity);
+	
+	String getLocale();
+	
+	int getPing();
+	
+	void sendActionBar(String text);
+	
 	
 }
---
+++ src/main/java/org/pipemc/TabList.java
@@ -0,0 +1,11 @@
+package org.pipemc;
+
+public interface TabList {
+
+	void setHeader(String header);
+	
+	void setFooter(String footer);
+	
+	void addEntry(String entry);
+	
+}
